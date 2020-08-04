a52bd3af0b73eb61d52b52eee22e50fed27e24cd
jomagsm <jomagsm@gmail.com>
2020-08-04 22:12:09 +0600

    Исправил базу и сделал миграцию

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,17 @@
+from django.apps import AppConfig
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Status, Task_type, Task
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    fields = ['name']
+
+class Task_typeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    fields = ['name']
+
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Task_type, Task_typeAdmin)
+admin.site.register(Task)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -3,22 +3,20 @@ from django.utils import timezone
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=15, verbose_name='Новая')
-    in_progress = models.CharField(max_length=15, verbose_name='В процессе')
-    done = models.CharField(max_length=15, verbose_name='Выполненно')
+    name = models.CharField(max_length=15, verbose_name='Статус')
 
+    def __str__(self): return self.name
 
 class Task_type(models.Model):
-    task = models.CharField(max_length=15, verbose_name='Задача')
-    bug = models.CharField(max_length=15, verbose_name='Ошибка')
-    enhancement = models.CharField(max_length=15, verbose_name='Улучшение')
+    name = models.CharField(max_length=15, verbose_name='Тип')
 
+    def __str__(self): return self.name
 
 class Task(models.Model):
-    summary = models.TextField(max_length= 300, verbose_name='Заголовок')
+    summary = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
     description = models.TextField(max_length= 3000, null=True, blank=True, verbose_name='Описание')
     status = models.ForeignKey('webapp.Status', related_name='status', on_delete=models.PROTECT, verbose_name='Статус')
-    type_task = models.ForeignKey('webapp.Task', related_name='type', on_delete=models.PROTECT, verbose_name='Тип')
+    type_task = models.ForeignKey('webapp.Task_type', related_name='type', on_delete=models.PROTECT, verbose_name='Тип')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
 
     def __str__(self):
