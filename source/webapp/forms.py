26af198f976327c6637a6a3a78dec4124da5ee4c
jomagsm <jomagsm@gmail.com>
2020-08-13 16:46:59 +0600

    Написал валидатцию на Недопустимые символы в Заголовке, и не короче 5 символов. 2 проверка описание не должна быть одинаковым и не короче Заголовка

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -5,54 +5,55 @@ from webapp.models import Task, Status, Task_type
 from django.core.validators import BaseValidator
 from django.utils.deconstruct import deconstructible
 
+INVALID_SIMVOLS= '~@#$%^-_(){}'''
 
-def at_least_10(string):
-    if len(string) < 10:
-        raise ValidationError('Value is too short!')
-
-
-@deconstructible
-class MinLengthValidator(BaseValidator):
-    message = 'Value "%(value)s" has length of %(show_value)d! It should be at least %(limit_value)d symbols long!'
-    code = 'too_short'
-
-    def compare(self, a, b):
-        return a < b
-
-    def clean(self, x):
-        return len(x)
+# @deconstructible
+# class MinLengthValidator(BaseValidator):
+#     message = 'Value "%(value)s" has length of %(show_value)d! It should be at least %(limit_value)d symbols long!'
+#     code = 'too_short'
+#
+#     def compare(self, a, b):
+#         return a < b
+#
+#     def clean(self, x):
+#         return len(x)
 
 
 class TaskForm(forms.ModelForm):
-    # summary = forms.CharField(widget=forms.TextInput(attrs={'class': "form-control"}))
-    # description = forms.CharField(widget=forms.Textarea(attrs={'class': "form-control"}))
-    # status = forms.ModelChoiceField(widget=forms.RadioSelect)
-    # type_task = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple)
 
     class Meta:
         model = Task
         fields = ['summary', 'description', 'status', 'type_task']
-        # widgets = {'summary': forms.CharField(widget=forms.TextInput(attrs={'class': "form-control"})),
-        #            'description': forms.CharField(widget=forms.Textarea(attrs={'class': "form-control"}))}
-                   # 'status': forms.ModelChoiceField(widget=forms.RadioSelect),
-                   # 'type_task': forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple)}
-
-
-    # summary = forms.CharField(max_length=200, required=True, label='Заголовок', widget=forms.TextInput(attrs={'class': "form-control"}))
-    # description = forms.CharField(max_length=3000, required=True, label='Описание',
-    #                               widget=forms.Textarea(attrs={'class': "form-control"}),
-    #                               validators=[MinLengthValidator(5)])
-    # status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Статус', empty_label=None,
-    #                                 widget=forms.RadioSelect)
-    # type_task = forms.ModelMultipleChoiceField(queryset=Task_type.objects.all(), required=True, label='Тип', widget=forms.CheckboxSelectMultiple)
+        widgets = {'summary': forms.TextInput(attrs={'class': "form-control"}),
+                   'description': forms.Textarea(attrs={'class': "form-control"}),
+                   'status': forms.RadioSelect,
+                   'type_task': forms.CheckboxSelectMultiple}
 
     def clean(self):
         cleaned_data = super().clean()
         errors = []
         summary = cleaned_data.get('summary')
         description = cleaned_data.get('description')
-        if summary == description:
-            errors.append(ValidationError('Text of the summary should not duplicate it"s description!'))
+        if summary:
+            if summary == description:
+                errors.append(ValidationError('Название и Описание не должны быть одинаковыми'))
+            if len(description) < len(summary):
+                errors.append('Описание не должны быть короче Заголовка')
+            if errors:
+                raise ValidationError(errors)
+        return cleaned_data
+
+    def clean_summary(self):
+        errors = []
+        summary = self.cleaned_data['summary']
+        invalid_sim = 0
+        for i in summary:
+            if i in INVALID_SIMVOLS:
+                invalid_sim += 1
+        if invalid_sim > 0:
+            errors.append(ValidationError(f'Вы использовали {invalid_sim} раз, недопустимые символы'))
+        if len(summary) < 5:
+            errors.append(ValidationError(f'Название задач не должны быть короче 5 символов'))
         if errors:
             raise ValidationError(errors)
-        return cleaned_data
+        return summary
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -23,8 +23,7 @@ TYPE_CHOICE2 = [Task_type.objects.all()]
 
 class Task(models.Model):
     summary = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
-    description = models.TextField(max_length= 3000, null=True, blank=True, verbose_name='Описание',
-                                   validators=[MinLengthValidator(5)])
+    description = models.TextField(max_length= 3000, null=True, blank=True, verbose_name='Описание')
     status = models.ForeignKey('webapp.Status', related_name='status', on_delete=models.PROTECT, verbose_name='Статус')
     type_task = models.ManyToManyField('webapp.Task_type', related_name='type', verbose_name='Тип')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
