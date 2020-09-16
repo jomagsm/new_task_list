7cca58d791a72aab1293b7ceac8cd22a90229618
jomagsm <jomagsm@gmail.com>
2020-09-16 22:38:21 +0600

    Написал все кроме проверок что он пользователь

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from accounts.views import RegisterView, UserDetailView,UserList
+from accounts.views import RegisterView, UserDetailView, UserList, UserChangeView, UserPasswordChangeView
 
 app_name = 'accounts'
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', RegisterView.as_view(), name='create'),
     path('<int:pk>/', UserDetailView.as_view(), name='detail'),
-    path('user_list/', UserList.as_view(),name='user_list')
+    path('user_list/', UserList.as_view(),name='user_list'),
+    path('<int:pk>/change/', UserChangeView.as_view(), name='change'),
+    path('<int:pk>/password_change', UserPasswordChangeView.as_view(), name='password_change')
 ]
